79fd5c4df38758410cf1c2e0734b2dddd7798d16
Cajamarquina <127974997+Cajamarquina@users.noreply.github.com>
2023-08-14 17:03:34 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+    }
 
+    let intervalId = null;
+    let lastBackgroundColor = '';
+    const startButton = document.querySelector('[data-start]');
+    const stopButton = document.querySelector('[data-stop]');
+    const body = document.body;
+
+    function changeBackgroundColor() {
+      const randomColor = getRandomHexColor();
+      body.style.backgroundColor = randomColor;
+    }
+
+    startButton.addEventListener('click', () => {
+      if (!intervalId) {
+        startButton.disabled = true;
+        changeBackgroundColor();
+        intervalId = setInterval(changeBackgroundColor, 1000); 
+      }
+    });
+
+    stopButton.addEventListener('click', () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+        lastBackgroundColor = body.style.backgroundColor;
+        startButton.disabled = false;
+      }
+      // Set the background color to the last recorded color when "Stop" was clicked
+      body.style.backgroundColor = lastBackgroundColor;
+    });
